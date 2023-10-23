0fb2fdcf7b745926cb7748ca150197eefcb64016
Nguyễn Trần Tấn Quy <n20dccn131@student.ptithcm.edu.vn>
2023-10-24 02:33:44 +0700

    Uodate Chức năng của trang Nhập hàng

--- WebThoiTranng/src/main/java/management/dao/INhapHangDao.java
+++ WebThoiTranng/src/main/java/management/dao/INhapHangDao.java
@@ -2,10 +2,13 @@ package management.dao;
 
 import java.util.List;
 
+import management.entity.Chatlieu;
 import management.entity.Ctpn;
 import management.entity.Ctsize;
+import management.entity.Loaimh;
 import management.entity.Mathang;
 import management.entity.Nhacungcap;
+import management.entity.Nhanhieu;
 import management.entity.Nhanvien;
 import management.entity.Phieunhap;
 import management.entity.Size;
@@ -21,6 +24,12 @@ public interface INhapHangDao{
 	public Ctsize layCtsize(int mamh, int masize);
 	
 	public List<Size> layDSSize();
+	
+	public List<Nhanhieu> layDSNhanHieu();
+	
+	public List<Chatlieu> layDSChatlieu();
+	
+	public List<Loaimh> layDSLoaiSP();
 	
 	public void themCtSize(int manh,int masize) ;
 
@@ -38,7 +47,7 @@ public interface INhapHangDao{
 	
 	public void themCtpn(List<Ctpn> ctpns);
 	
-	
+	public void themSPMoi(Mathang mh) ;
 	
 	
 	
